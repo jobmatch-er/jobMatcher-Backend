5438b621253212a6185d53c7ab08d218c8ec4d09
Jakob-Niklas See <blendercraft.info@gmail.com>
2019-07-08 11:48:26 +0200

    Initial commit, added Main.java, BackendInstance and the Java-Websocket- libary for communication with the api

---
+++ src/de/wecodeit/jobmatcher/BackendInstance.java
@@ -0,0 +1,14 @@
+package de.wecodeit.jobmatcher;
+
+public class BackendInstance extends Thread
+{
+    public BackendInstance()
+    {
+
+    }
+
+    public void run()
+    {
+
+    }
+}
---
+++ src/de/wecodeit/jobmatcher/Main.java
@@ -0,0 +1,10 @@
+package de.wecodeit.jobmatcher;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        BackendInstance jobMatcher = new BackendInstance();
+        jobMatcher.start();
+    }
+}

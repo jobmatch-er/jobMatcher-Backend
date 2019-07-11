d5dc9e3274fcde9cdb157ec6f8e7217bbb0b64e3
Jakob-Niklas See <blendercraft.info@gmail.com>
2019-07-11 09:09:15 +0200

    Added convertToUTF8 to FormatUtil

--- src/de/jakobniklas/util/FormatUtil.java
+++ src/de/jakobniklas/util/FormatUtil.java
@@ -39,4 +39,19 @@ public class FormatUtil
             return false;
         }
     }
+
+    public static String convertToUTF8(String s)
+    {
+        String out = null;
+        try
+        {
+            out = new String(s.getBytes("UTF-8"), "ISO-8859-1");
+        }
+        catch(java.io.UnsupportedEncodingException e)
+        {
+            return null;
+        }
+
+        return out;
+    }
 }
